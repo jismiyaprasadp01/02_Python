3f7077d23a2b78bca2e25bc845bdda870e99c367
jismiya <jismiyaprasadp01@gmail.com>
2026-02-25 22:26:03 +0530

    25 Feb

---
+++ 04_Loops/worksheet1/01_PrintDigits.py
@@ -0,0 +1,10 @@
+#Print all 3-digit numbers such that the sum of the cubes of their digits equals the number itself.
+for i in range(100,1000):
+    j=i
+    s=0
+    while j!=0:
+        m = j%10
+        s = s + (m**3)
+        j//=10
+    if i==s:
+        print(i)
